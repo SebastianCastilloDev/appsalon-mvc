1482fdc01e84d7701c216be7a2a7f92c806823a4
Sebastian Castillo <tantrum5535@gmail.com>
2023-07-04 12:34:31 -0400

    autenticacion de usuarios

--- models/usuario.php
+++ models/usuario.php
@@ -50,6 +50,17 @@ class Usuario extends ActiveRecord{
         return self::$alertas;
     }
 
+    public function validarLogin() {
+        if (!$this->email){
+            self::$alertas['error'][] = "El email es obligatorio";
+        }
+        if (!$this->password) {
+            self::$alertas['error'][] = "El password es obligatorio";
+        }
+
+        return self::$alertas;
+    }
+
     // Validar si un usuario existe
     public function existeUsuario(){
         $query = " SELECT * FROM " . self::$tabla . " WHERE email = '" . $this->email . "' LIMIT 1";
@@ -68,5 +79,15 @@ class Usuario extends ActiveRecord{
 
     public function crearToken(){
         $this->token = uniqid();
+    }
+
+    public function comprobarPasswordAndVerificado($password) {
+        $resultado = password_verify($password, $this->password);
+
+        if(!$resultado || !$this->confirmado){
+            self::$alertas['error'][] = "Password incorrecto o tu cuenta no ha sido confirmada";
+        }else{
+            return true;
+        }
     }
 }
